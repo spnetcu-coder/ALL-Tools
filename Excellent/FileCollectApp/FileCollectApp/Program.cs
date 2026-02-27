575d53653c553b77f9bb01dfaf1075365c8ee470
spnetcu-coder <spnetcu@gmail.com>
2026-02-27 17:51:53 +0900

    Revert "ファイル収集ツール：検索・収集機能"
    This reverts commit 7b0fcff6deeff5606b1626ab43fbe0786276f9ee.

--- Excellent/FileCollectApp/FileCollectApp/Program.cs
+++ Excellent/FileCollectApp/FileCollectApp/Program.cs
@@ -16,7 +16,7 @@ namespace FileCollectApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FileCollect());
+            Application.Run(new Form1());
         }
     }
 }
